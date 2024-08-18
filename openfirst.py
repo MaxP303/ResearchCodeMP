d2fd4ab5d6cd47888ada61d391f4a526fb3bbfb3
MaxP303 <max.palsgraaf@live.nl>
2024-08-18 15:00:21 +0200

    Openfirst.py

--- openfirst.py
+++ openfirst.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-# Step 1: Read the first 100,000 rows
-file_path = r"D:\Documents\ThesisD\counted_nodupli_datafinal2_1.csv"
+# Step 1: Read the first 2.000 rows
+file_path = r"C:\yourpath\data\rawdata\data.csv"
 df = pd.read_csv(file_path, nrows=2000)
 
 # Step 2: Save to a new CSV file
-output_file_path = r"D:\Documents\ThesisD\first2000with_outliers_data.csv"
+output_file_path = r"C:\yourpath\data\first2000_data.csv"
 df.to_csv(output_file_path, index=False)
